334eb9a3cc60a39692b8d1c0dc6366212d3ff37b
royz <yzheng77@students.cs.ubc.ca>
2025-02-15 22:07:52 -0800

    added the ComplexNumber Class equals comparison override and tests

--- src/main/model/ComplexNumber.java
+++ src/main/model/ComplexNumber.java
@@ -20,12 +20,12 @@ public class ComplexNumber {
     }
 
     //MODIFIES: this
-    //EFFECT: Multiplies two complex numbers together and return the result as a new Complex number
+    //EFFECT: Multiplies the current complex numbers together with another and return the result as a new Complex number
     public ComplexNumber multiply(ComplexNumber otherNumber){
         double resultReal;
         double resultImaginary;
 
-        resultReal = (real * otherNumber.real - imaginary) * (otherNumber.imaginary);
+        resultReal = (real * otherNumber.real) - (imaginary * otherNumber.imaginary);
         resultImaginary = (real * otherNumber.imaginary) + (imaginary * otherNumber.real);
         
 
@@ -33,7 +33,7 @@ public class ComplexNumber {
     }
 
     //MODIFIES: this
-    //EFFECTS: adds two complex numbers together and returns the result as a new complex number
+    //EFFECTS: adds the current complex numbers together with an other and returns the result as a new complex number
     public ComplexNumber add(ComplexNumber otherNumber){
         double resultReal;
         double resultImaginary;
@@ -42,5 +42,19 @@ public class ComplexNumber {
 
         return new ComplexNumber(resultReal, resultImaginary);
     }
+
+    
+    //EFFECTS: allows for a very small margin of error to account of floating point errors
+    //overrides the equals comparison to return true when errors is smaller then 1e-10
+    //everything else is the same as a regular equals 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ComplexNumber that = (ComplexNumber) obj;
+
+    return Math.abs(this.real - that.real) < 1e-10 && Math.abs(this.imaginary - that.imaginary) < 1e-10;
+}
+
 
 }
---
+++ src/test/model/TestComplexNumber.java
@@ -0,0 +1,85 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestComplexNumber {
+    private double alpha1;
+    private double beta1;
+
+    private double alpha2;
+    private double beta2;
+
+    private double alpha3;
+    private double beta3;
+
+    private ComplexNumber cnum1;
+    private ComplexNumber cnum2;
+    private ComplexNumber cnum3;
+    private ComplexNumber cnum4;
+
+    private Qubit qubit;
+
+    
+    @BeforeEach
+    void runBefore() {
+        alpha1 = 0;
+        beta1 = 1;
+        cnum1 = new ComplexNumber(alpha1, beta1);
+
+        alpha2 = 0.5;
+        beta2 = 0.5;
+        cnum2 = new ComplexNumber(alpha2, beta2);
+
+        alpha3 = 1.0;
+        beta3 = 0;
+        cnum3 = new ComplexNumber(alpha3, beta3);
+
+        cnum4 = new ComplexNumber(alpha1, beta1); //this is set to the same as cnum1 to test the equals override
+        qubit = new Qubit(cnum1, cnum2);// another class to test the override equals
+    }
+
+
+    @Test
+    void testComplexNumberConstructor() {
+        assertEquals(cnum1.real, 0.0);
+        assertEquals(cnum1.imaginary, 1.0);
+    }
+    
+    @Test
+    void testMagnitudeSquared(){
+        assertEquals(cnum1.magnitudeSquared(), 1);
+        assertEquals(cnum2.magnitudeSquared(), 0.5);
+        assertEquals(cnum3.magnitudeSquared(), 1);
+    }
+
+    @Test
+    void testMultiply(){
+        assertEquals(cnum1.multiply(cnum2), new ComplexNumber(-0.50, 0.50));
+        assertEquals(cnum2.multiply(cnum2), new ComplexNumber(0, 0.5));
+        assertEquals(cnum3.multiply(cnum2),  new ComplexNumber(0.50, 0.50));
+    }
+
+    @Test
+    void testAdd(){
+        assertEquals(cnum1.add(cnum2), new ComplexNumber(0.50, 1.50));
+        assertEquals(cnum2.add(cnum2), new ComplexNumber(1.0, 1.0));
+        assertEquals(cnum3.add(cnum2), new ComplexNumber(1.50, 0.50));
+    }
+
+    @Test
+    void testEqualsOverride(){
+        assertTrue(cnum1.equals(cnum1));
+        assertTrue(cnum1.equals(cnum4));
+        assertFalse(cnum1.equals(cnum2));
+        
+        assertFalse(cnum1.equals(null));
+        assertFalse(cnum1.equals(qubit));
+
+        assertTrue(cnum1.equals(new ComplexNumber(0, 1.00000000001)));
+        assertFalse(cnum1.equals(new ComplexNumber(0, 1.0000000001)));
+    }
+
+}
