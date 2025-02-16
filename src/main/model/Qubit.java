9ced082251ac0e928b9892d42e339f590f9b7f07
royz <yzheng77@students.cs.ubc.ca>
2025-02-16 05:04:25 -0800

    added second empty constructor for qubit, created method measure and tests for the qubit class (used a seeded random to check the randomness of the measure method)

--- src/main/model/Qubit.java
+++ src/main/model/Qubit.java
@@ -13,10 +13,50 @@ public class Qubit {
     private ComplexNumber beta; // this is the probability amplitude of |1> 
 
     //REQUIRES alpha^2 + beta^2 must = 1 to ensure that the state is normalized 
-    public Qubit(ComplexNumber alpha, ComplexNumber beta) {
+    public Qubit() {
         this.alpha = new ComplexNumber(1, 0);
         this.beta = new ComplexNumber(0, 0);
     }
 
+    public Qubit(ComplexNumber alpha, ComplexNumber beta) {
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: iterates through the rows(that are max two because only a 2x2 matrix is allowed), and multiples 
+    //the rows by the Gate that is defined in the Gate class
+    public void applyGate(Gate gate){
+        ComplexNumber[] newState = new ComplexNumber[2];
+        for (int i = 0; i < 2; i++){
+            newState[i] = gate.multiplyMatrixRow(i, alpha, beta);
+        }
+        this.alpha = newState[0];
+        this.beta = newState[1];
+    }
+
+    public ComplexNumber getAlpha(){
+        return this.alpha;
+    }
+
+    public ComplexNumber getBeta(){
+        return this.beta;
+    }
+
+    //MODIFIES: this 
+    //EFFECTS: this method collapsese the qubit into either |0> or |1> by comparing
+    //a random number with the square of the probability amplitude of |0>, if the random 
+    //is smaller, then it collapses to |0>, else |1>
+    public void measure(Random RND){
+        double probability = this.alpha.magnitudeSquared();
+        if(RND.nextDouble() < probability){
+            this.alpha = new ComplexNumber(1, 0);
+            this.beta = new ComplexNumber(0, 0);
+        }
+        else {
+            this.alpha = new ComplexNumber(0,0);
+            this.beta = new ComplexNumber(1, 0);
+        }
+    }
 
 }
---
+++ src/test/model/TestQubit.java
@@ -0,0 +1,104 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+public class TestQubit {
+    private Qubit testQubit;
+    private Qubit testQubit1;
+    private Qubit testQubit2;
+    private Qubit testQubit3;
+    private Qubit testQubit0;
+    private ComplexNumber alpha;
+    private ComplexNumber alpha1;
+    private ComplexNumber alpha2;
+    private ComplexNumber alpha3;
+    private ComplexNumber beta;
+    private ComplexNumber beta1;
+    private ComplexNumber beta2;
+    private ComplexNumber beta3;
+    private Random RND;
+
+    @BeforeEach
+    void runBefore(){
+        RND = new Random();
+
+        alpha = new ComplexNumber(1, 0);
+        beta = new ComplexNumber(0, 0);
+
+        alpha1 = new ComplexNumber(0.7, 0.3);
+        beta1 = new ComplexNumber(0.4, 0.6);
+
+        alpha2 = new ComplexNumber(0, 0);
+        beta2 = new ComplexNumber(1, 0);
+        
+        alpha3 = new ComplexNumber(1/Math.sqrt(2), 0);
+        beta3 = new ComplexNumber(0, 1/Math.sqrt(2));    
+
+        testQubit0 = new Qubit();
+        testQubit = new Qubit(alpha, beta);
+        testQubit1 = new Qubit(alpha1, beta1);
+        testQubit2 = new Qubit(alpha2, beta2);
+        testQubit3 = new Qubit(alpha3, beta3);
+    }
+
+    @Test 
+    void testConstructor(){
+        assertEquals(alpha1, testQubit1.getAlpha());
+        assertEquals(beta1, testQubit1.getBeta());
+        assertEquals(alpha, testQubit0.getAlpha());
+        assertEquals(beta, testQubit0.getBeta());
+    }
+    @Test
+    void testApplyHadamardGate() {
+        testQubit.applyGate(Gate.HADAMARD);
+
+        assertEquals(testQubit.getAlpha(), new ComplexNumber(1 / Math.sqrt(2), 0));
+        assertEquals(testQubit.getBeta(), new ComplexNumber(1 / Math.sqrt(2), 0));
+    }
+
+    @Test
+    void testApplyBitFlipGate() {
+        testQubit.applyGate(Gate.BITFLIP);
+
+        assertEquals(testQubit.getAlpha(), new ComplexNumber(0, 0));
+        assertEquals(testQubit.getBeta(), new ComplexNumber(1, 0));
+    }
+
+    @Test
+    void testApplyPhaseFlipGate() {
+        testQubit.applyGate(Gate.PHASEFLIP);
+
+        assertEquals(testQubit.getAlpha(), new ComplexNumber(1, 0));
+        assertEquals(testQubit.getBeta(), new ComplexNumber(0, 0));
+    }
+
+    @Test
+    void testMeasure(){
+        testQubit.measure(RND);
+
+        assertEquals(testQubit.getAlpha(), new ComplexNumber(1, 0));
+        assertEquals(testQubit.getBeta(), new ComplexNumber(0, 0));
+
+        Random RND2 = new Random(10);
+        testQubit1.measure(RND2);
+
+        assertEquals(testQubit1.getAlpha(), new ComplexNumber(0, 0));
+        assertEquals(testQubit1.getBeta(), new ComplexNumber(1, 0));
+
+        Random RND3 = new Random(80);
+        testQubit2.measure(RND3);
+        assertEquals(testQubit2.getAlpha(), new ComplexNumber(0, 0));
+        assertEquals(testQubit2.getBeta(), new ComplexNumber(1, 0));
+
+        Random RND4 = new Random(2156);
+        testQubit3.measure(RND4);
+        assertEquals(testQubit3.getAlpha(), new ComplexNumber(0, 0));
+        assertEquals(testQubit3.getBeta(), new ComplexNumber(1, 0));
+
+    }
+
+}
